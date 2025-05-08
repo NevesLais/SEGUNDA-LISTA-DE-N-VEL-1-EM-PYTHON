268873ca415e0379de82ca938aad5df104084b1a
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-08 20:17:09 -0300

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,7 +1,8 @@
-numero = int(input("Digite um número inteiro")
- if(numero >0):
+numero = int(input("Digite um número inteiro: "))  
+
+if numero > 0:  
     print("Número positivo")
-elif(numero <0):
-     print("Número negativo")
-else: 
-     print("Número neutro")
+elif numero < 0:  
+    print("Número negativo")
+else:
+    print("Número neutro")
