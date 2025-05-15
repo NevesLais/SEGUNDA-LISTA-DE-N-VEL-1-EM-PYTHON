055188df05f74cb420eaa62d169a0c22fb60d3e4
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-15 19:16:00 -0300

    ex4.py

--- ex4.py
+++ ex4.py
@@ -1,19 +1,24 @@
-salario= float(input("Digite o seu salário:\n"))
-if(salario<=280):
+salario = float(input("Digite o seu salário:\n"))
+
+if salario <= 280:
     percentualAumento = 20
     aumento = (salario * percentualAumento) / 100
     novoSalario = salario + aumento
 
-elif (salario >280 and salario <=700):
-     percentualAumento = 15
-     aumento = (salario * percentualAumento) / 100
-     novoSalario = salario + aumento
+elif salario > 280 and salario <= 700:
+    percentualAumento = 15
+    aumento = (salario * percentualAumento) / 100
+    novoSalario = salario + aumento
 
-elif(salario >700 and salario <=1500):
-     percentualAumento = 10
-     aumento = (salario * percentualAumento) / 100
-     
+elif salario > 700 and salario <= 1500:
+    percentualAumento = 10
+    aumento = (salario * percentualAumento) / 100
+    novoSalario = salario + aumento 
 
-elif(salario >1500):
-     percentualAumento = 5
-     aumento = (salario * percentualAumento) / 100
+elif salario > 1500:
+    percentualAumento = 5
+    aumento = (salario * percentualAumento) / 100
+    novoSalario = salario + aumento
+
+print("Seu aumento foi de R$", aumento)
+print("Seu novo salário é R$", novoSalario)
