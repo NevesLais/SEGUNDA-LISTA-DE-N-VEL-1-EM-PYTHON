9cbb998eae8c7e9137abec14b3ea5c201a71aa2d
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-20 22:39:48 -0300

    ex6.py

--- ex6.py
+++ ex6.py
@@ -1,1 +1,30 @@
 
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+
+if numero1 == numero2 == numero3:
+    print("Todos os números são iguais.")
+else:
+    
+    if numero1 >= numero2 and numero1 >= numero3:
+        maior = numero1
+    elif numero2 >= numero1 and numero2 >= numero3:
+        maior = numero2
+    else:
+        maior = numero3
+
+    if numero1 <= numero2 and numero1 <= numero3:
+        menor = numero1
+    elif numero2 <= numero1 and numero2 <= numero3:
+        menor = numero2
+    else:
+        menor = numero3
+
+    print("O maior número é " + str(maior) + ".")
+    print("O menor número é " + str(menor) + ".")
+
+    
+    if numero1 == numero2 or numero1 == numero3 or numero2 == numero3:
+        print("Há números iguais.")
