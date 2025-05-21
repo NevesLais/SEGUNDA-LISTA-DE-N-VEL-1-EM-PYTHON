583fe48c83c52f40621e1d247780187c8aa6d48f
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-20 22:40:39 -0300

    ex7.py

---
+++ ex7.py
@@ -0,0 +1,12 @@
+
+a = float(input("Digite o primeiro lado do triângulo: "))
+b = float(input("Digite o segundo lado do triângulo: "))
+c = float(input("Digite o terceiro lado do triângulo: "))
+
+
+if a == b and b == c:
+    print("Triângulo Equilátero")
+elif a == b or b == c or a == c:
+    print("Triângulo Isósceles")
+else:
+    print("Triângulo Escaleno")
